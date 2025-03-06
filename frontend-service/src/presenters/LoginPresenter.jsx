997fc0c9f1a4266fe0358516e8de5f222acb79fc
Diddi <diddi.dutta@outlook.com>
2025-03-06 18:49:35 +0100

    able to logout

--- frontend-service/src/presenters/LoginPresenter.jsx
+++ frontend-service/src/presenters/LoginPresenter.jsx
@@ -2,14 +2,10 @@ import LoginView from "@/views/LoginView";
 import LoginSuccessView from "@/views/LoginSuccessView";
 import { useStore } from "vuex";
 
-
 export default function LoginPresenter(props) {
     const store = useStore();
     return <div class="main">
-                <LoginView model={props.model} store={store}
-                           reactiveUsername={props.model.username}
-                           reactivePassword={props.model.password}
-                           isLoggedIn={props.model.loggedIn}
-
+                <LoginView model={props.model}
+                           store={store}
            /></div>;
 }
--- frontend-service/src/views/NavbarView.jsx
+++ frontend-service/src/views/NavbarView.jsx
@@ -1,10 +1,36 @@
-export default function NavbarView(props) {
-    return (<div class="navbar"> 
-    <RouterLink to="/login">Login</RouterLink>
-    <RouterLink to="/register">Register</RouterLink>
-    <RouterLink to="/apply">Apply</RouterLink>
-    <RouterLink to="/applications">View applications</RouterLink>
-    <RouterLink to="/manage-ads">Manage ads</RouterLink>
-    </div>
-    );
+import { ref, computed} from 'vue'
+import { useRouter } from "vue-router";
+
+export default function NavbarView({store}) {
+    const loading = ref(false);
+    const message = ref("");
+    const router = useRouter();
+    const loggedIn = computed(() => store.state.auth.status.loggedIn);
+
+    const logoutACB = async (event) => {
+        if(loggedIn.value) {
+            try {
+                await store.dispatch("auth/logout");
+                router.push("/login");
+            } catch (error) {
+                loading.value = false;
+                message.value = error.response?.data?.message || error.message || error.toString();
+            }
+        }
+    }
+
+    if(loggedIn.value) {
+        return (<div class="navbar">
+                    <button onClick={logoutACB}>Logout</button>
+                    <RouterLink to="/apply">Apply</RouterLink>
+                    <RouterLink to="/applications">View applications</RouterLink>
+                </div>)
+        ;
+    } else {
+        return  (<div class="navbar">
+                     <RouterLink to="/login">Login</RouterLink>
+                     <RouterLink to="/register">Register</RouterLink>
+                 </div>)
+        ;
+    }
 }
