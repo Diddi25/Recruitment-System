ee2f6abc0047b189d314ba318890e563e7facedd
Diddi <diddi.dutta@outlook.com>
2025-03-09 22:00:00 +0100

    shall pull down latest frontend

--- frontend-service/src/views/LoginView.jsx
+++ frontend-service/src/views/LoginView.jsx
@@ -5,15 +5,15 @@ export default function LoginView(props) {
     let password = "";
 
     return (<div class="login">
-        <h2>Log in</h2>
-        <h4>Username</h4>
-        <div class="text-input"><input type="text" onInput={onUsernameInput}>
-        </input></div>
-        <h4>Password</h4>
-        <div class="text-input"><input type="text" onInput={onPasswordInput}></input></div>
-        {getIncorrectCredentialsWarning()}
-        <button onClick={onSubmit}>Submit</button>
-    </div>);
+                <h2>Log in</h2>
+                <h4>Username</h4>
+                <div class="text-input"><input type="text" onInput={onUsernameInput}>
+                </input></div>
+                <h4>Password</h4>
+                <div class="text-input"><input type="text" onInput={onPasswordInput}></input></div>
+                {getIncorrectCredentialsWarning()}
+                <button onClick={onSubmit}>Submit</button>
+            </div>);
 
     function onUsernameInput(input){
         username = input.target.value;
